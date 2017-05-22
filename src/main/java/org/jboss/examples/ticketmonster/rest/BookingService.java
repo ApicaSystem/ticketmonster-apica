44d236c2c7948689328811efdb77882311c9382d
abdiapica <abdiqani.hassan@apicasystem.com>
2017-05-22 13:45:41 +0200

    Remove sleep

--- src/main/java/org/jboss/examples/ticketmonster/rest/BookingService.java
+++ src/main/java/org/jboss/examples/ticketmonster/rest/BookingService.java
@@ -123,7 +123,7 @@ public class BookingService extends BaseEntityService<Booking> {
     @Consumes(MediaType.APPLICATION_JSON)
     public Response createBooking(BookingRequest bookingRequest) {
         try {
-            Thread.sleep(10000);
+            //Thread.sleep(10000);
             // identify the ticket price categories in this request
             Set<Long> priceCategoryIds = bookingRequest.getUniquePriceCategoryIds();
             
