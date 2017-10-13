d5dfecca7d82677e8d90fd3735cf1da078df7584
abdiapica <abdiqani.hassan@apicasystem.com>
2017-10-13 07:37:52 +0200

    Update BookingService.java

--- src/main/java/org/jboss/examples/ticketmonster/rest/BookingService.java
+++ src/main/java/org/jboss/examples/ticketmonster/rest/BookingService.java
@@ -125,7 +125,7 @@ public class BookingService extends BaseEntityService<Booking> {
     public Response createBooking(BookingRequest bookingRequest) {
         try {
             // Add 6 second sleep in booking flow
-            // Thread.sleep(6000);
+             Thread.sleep(7000);
 
             // identify the ticket price categories in this request
             Set<Long> priceCategoryIds = bookingRequest.getUniquePriceCategoryIds();
